5d644fbb43da5b0b14fe097cd5d8033b87281373
Mircea <mircea.prodan@yahoo.com>
2018-07-12 15:33:55 +0300

    Adaugare de paginare la blog

--- blog2/views.py
+++ blog2/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, get_object_or_404 
 from django.utils import timezone
-#from django.http import HttpResponse
 from . models import Post
-#from django.urls import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-	return render(request, 'blog2/post_list.html', {'posts':posts})
+	object_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	paginator = Paginator(object_list, 3)  #3 postari pe pagina
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+	return render(request, 'blog2/post_list.html', {'page' : page, 'posts' : posts})
+
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog2/post_detail.html', {'post': post})
+
